aa4107f2547c344c9286e6dec80874d7cd7cea47
Kasia <kasia.bondarava@gmail.com>
2021-06-30 09:08:11 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const searchBtn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+
+
+searchBtn.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+})
