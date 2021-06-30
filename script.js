4f4cc368625e549fc962f11aa67df294e28cdfcf
Kasia <kasia.bondarava@gmail.com>
2021-06-30 09:38:22 -0400

    Hide search on click outside of the input field

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const searchBtn = document.querySelector(".btn");
+// Select the entire search container
 const search = document.querySelector(".search");
+// Select input field
 const input = document.querySelector(".input");
+// Select search button element
+const searchBtn = document.querySelector(".btn");
+// Select icon on search button element
+const searchIcon = document.querySelector(".fa-search");
+// Select the entire HTML document
+const html = document.querySelector("html");
 
 
+// Event Listeners
+// Toggle active class on click
 searchBtn.addEventListener("click", () => {
     search.classList.toggle("active");
     input.focus();
+})
+
+// Remove active class from the element when clicking outside of the input and search button
+html.addEventListener("click", (e) => {
+    if (e.target !== input && e.target !== searchIcon && e.target !== searchBtn) {
+            search.classList.remove("active");
+    }
 })
